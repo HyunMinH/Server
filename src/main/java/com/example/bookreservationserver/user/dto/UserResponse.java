0efd32c8c8c71d7afa23f3ef5ca2faf4e34d863d
hyun.min.han <um4825@gmail.com>
2021-05-31 00:24:49 +0900

    now borrowing api return borrowing and expired

--- src/main/java/com/example/bookreservationserver/user/dto/UserResponse.java
+++ src/main/java/com/example/bookreservationserver/user/dto/UserResponse.java
@@ -3,12 +3,9 @@ package com.example.bookreservationserver.user.dto;
 import com.example.bookreservationserver.user.domain.aggregate.User;
 import com.example.bookreservationserver.user.domain.aggregate.UserType;
 import lombok.NoArgsConstructor;
-import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 
 @NoArgsConstructor
 public class UserResponse {
