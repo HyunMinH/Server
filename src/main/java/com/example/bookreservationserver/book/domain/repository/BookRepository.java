a0bc6db60477ef2508140d00ea32a5abab1c18bc
HyunMinH <um4825@gmail.com>
2023-01-03 13:27:41 +0900

    임시로 폴더 삭제

--- src/main/java/com/example/bookreservationserver/book/domain/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bookreservationserver.book.domain.repository;
-
-import com.example.bookreservationserver.book.domain.aggregate.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long>, BookRepositoryCustom {
-
-}
